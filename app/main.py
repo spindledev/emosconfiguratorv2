29cf9e90613eb1fd088c2b9650c5e9d3a590e81b
spindledev <44621646+spindledev@users.noreply.github.com>
2025-06-30 20:47:52 +0200

    Add OCC command helper functions

--- app/main.py
+++ app/main.py
@@ -1,4 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
+from . import occ_wrapper
+
 app = FastAPI()
-@app.get('/')
-def read_root(): return {'msg': 'ok'}
+
+
+@app.get("/")
+def read_root():
+    return {"msg": "ok"}
+
+
+@app.get("/camera/{parameter}")
+def get_camera_parameter(parameter: str):
+    try:
+        return occ_wrapper.read_parameter(parameter)
+    except occ_wrapper.OCCError as exc:
+        raise HTTPException(status_code=500, detail=str(exc))
+
+
+@app.post("/camera/{parameter}")
+def set_camera_parameter(parameter: str, value: str):
+    try:
+        return occ_wrapper.set_parameter(parameter, value)
+    except occ_wrapper.OCCError as exc:
+        raise HTTPException(status_code=500, detail=str(exc))
